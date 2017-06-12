4158c8751b3d8dc05c10d62406b22140d46a7c5d
Zepri <clem.collongues@gmail.com>
2017-06-12 10:17:38 +0200

    First commit

---
+++ db/migrate/20170607091429_create_indefinitions.rb
@@ -0,0 +1,11 @@
+class CreateIndefinitions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :indefinitions do |t|
+      t.string :name
+      t.text :description
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
